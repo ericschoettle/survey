c43bd09a3ef4afef669e2275a9225a48e51fb5d2
Eric Schoettle and Max Reichman <eric+max@>
2017-03-20 16:28:05 -0700

    Add functions for the arithmatic

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+  // var add = function(number1, number2) {
+  //   return number1 + number2;
+  // };
+  //
+  // var subtract = function(number1, number2) {
+  //   return number1 - number2;
+  // };
+  //
+  // var multiply = function(number1, number2) {
+  //   return number1 * number2;
+  // };
+  //
+  // var divide = function(number1, number2) {
+  //   return number1 / number2;
+  // };
+  //
+  // var number1 = parseInt(prompt("Enter a number:"));
+  // var number2 = parseInt(prompt("Enter another number:"));
+  // var result = multiply(number1, number2);
+  // alert(result);
+
+  // var extractFeetAndInchesFromText = function(height) {
+  //   var str = "6 foot 1";
+  //   var res = str.match(/\d/g);
+  // }
+  //
+  // var convertFeetAndInchesToInches = function(feet, inches) {
+  //   return feet * 12 + inches;
+  // };
+  //
+  // var convertInchesToMeters = function(heightInInches) {
+  //   return heightInInches / 39.37;
+  // };
+  //
+  // var convertPoundsToKilograms = function(weightInPounds) {
+  //   return weightInPounds / 2.2;
+  // };
+  //
+  // var calculateBMI = function(heightInMeters, weightInKilograms) {
+  //   return weightInKilograms / (heightInMeters ^ 2 ;)
+  // };
+
+  var str = "6 foot 1";
+  var res = str.split([/\s/]);
+  alert(res);
+
+});
