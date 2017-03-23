e7adf4bed1ebe035600b6125b62a9e77837863fb
Brian Fan and Eric Schoettle <brian+eric@>
2017-03-22 19:27:33 -0700

    made appointments toggle info based on user name

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,28 @@
 $(document).ready(function(){
   $("form#survey").submit(function(event){
     event.preventDefault();
-    // Full Name
-    var fullName = $("input#full-name").val();
-    console.log(fullName);
 
-    //Favorite Food
+    // define variables
+    var firstName = $("input#first-name").val();
     var food = $("select#favorite-foods").val();
-    console.log(food);
-
-    //Favorite Music
     var music = $("input:radio[name=music]:checked").val();
-    console.log(music);
-    
-    //Color
     var favoriteColor = $("input#color").val();
-    console.log(favoriteColor);
-
-    //Date
     var date = $("input#date").val();
-    console.log(date);
+    var time = $("input#time").val();
+
+    // contact
+    $("ul.contact").append("<li><a class='' href=\"#" + firstName + "\"><h3>Name: " + firstName + "</h3></a></li>");
+
+    // info
+    $("div.info .jumbotron").append("<div id=\"" + firstName + "\"><h3>" + firstName + "</h3> Food: " + food + " Music: " + music + " Color: " + favoriteColor + " Date:" + date + " time " + time + "</div>");
+
+    // show info
+    $(".contact a").click(function() {
+      $(this.hash).toggle();
+    });
+
+    // $("ul.contact").children("li").click(function() {
+    //   $("div[class~='firstName']").show();
+    // });
   });
 });
